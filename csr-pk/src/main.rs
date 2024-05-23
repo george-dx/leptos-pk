9f26c2f8593d7fb4b748bf01f43b996be4d5a50b
George Daian <georgedaianx@gmail.com>
2024-05-23 15:23:24 +0300

    Implement a basic component

---
+++ csr-pk/src/main.rs
@@ -0,0 +1,17 @@
+use leptos::*;
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button on:click=move |_| {
+            set_count.update(|n| *n += 1);
+        }>"Click me: " {count}</button>
+    }
+}
+
+fn main() {
+    console_error_panic_hook::set_once();
+    mount_to_body(|| view! { <App/> })
+}
