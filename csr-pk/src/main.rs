945214732f163036cfc8960365c434fff23bce91
George Daian <georgedaianx@gmail.com>
2024-06-11 16:44:38 +0300

    Error handling in action

--- csr-pk/src/main.rs
+++ csr-pk/src/main.rs
@@ -189,7 +189,40 @@ fn ControlFlow() -> impl IntoView {
     }
 }
 
+#[component]
+fn NumericInput() -> impl IntoView {
+    let (value, set_value) = create_signal(Ok(0));
+
+    let on_input = move |ev| set_value(event_target_value(&ev).parse::<i32>());
+
+    view! {
+        <h1>"Error Handling"</h1>
+        <label>
+            "Type a number (or not!)"
+            <input type="number" on:input=on_input/>
+            <ErrorBoundary
+                fallback=|errors| view! {
+                    <div class="error">
+                        <p>"Not a number! Errors: "</p>
+                        <ul>
+                            {
+                                move || errors.get().into_iter().map(|(_, e)| view! {<li>{e.to_string()}</li>}).collect_view()
+                            }
+                        </ul>
+                    </div>
+                }
+            >
+            <p>
+                "You entered "
+                <strong>{value}</strong>
+            </p>
+            </ErrorBoundary>
+        </label>
+    }
+}
+
+
 fn main() {
     console_error_panic_hook::set_once();
-    mount_to_body(|| view! { <ControlFlow/> })
+    mount_to_body(|| view! { <NumericInput/> })
 }
